7baf82147d2df9cf8e48b6e7f5758f863eae890e
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2022-01-14 17:49:18 -0500

    Create cop2001Lecture1.cpp

---
+++ cop2001Lecture1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  /* Lets create a quick variable
+     manipulate it. And then print
+     it out.
+  */
+  
+  // declare and assign variables (name for this is?)
+  int age = 21;
+  int year_of_birth = 2001
+
+  // print with cout - yes use the std:: why? 
+  // (or using std::cout declarative)
+  std::cout << "I was born in " << year_of_birth << "\n";
+  // or we could use printf - why might we?
+  printf("As I am {%d} I can get in\n", age);
+} 
