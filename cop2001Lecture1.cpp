d3ca2ec6100b348ca3d2d71da9a8ecdda7dd5ea4
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2022-01-18 12:53:46 -0500

    Update cop2001Lecture1.cpp

--- cop2001Lecture1.cpp
+++ cop2001Lecture1.cpp
@@ -1,18 +1,23 @@
+
 #include <iostream>
 
+// Quick test of compiler
+// Print out uid and greeting
+// print out current gpa
 int main() {
-  /* Lets create a quick variable
-     manipulate it. And then print
-     it out.
-  */
-  
-  // declare and assign variables (name for this is?)
-  int age = 21;
-  int year_of_birth = 2001
 
-  // print with cout - yes use the std:: why? 
-  // (or using std::cout declarative)
-  std::cout << "I was born in " << year_of_birth << "\n";
-  // or we could use printf - why might we?
-  printf("As I am {%d} I can get in\n", age);
+  int uid=125496;
+  std::cout << "Hello student " << uid << "\n";
+  printf("Hello student %d\n", uid);
+
+  double gpa = 3.7;
+  bool passing = true;
+  std::cout << "You have " << gpa << ". You are passing: "
+    << passing << "\n";
+  printf("You have %f. You are passing: %d\n", gpa, passing);
+  /*
+   * To fix the printf 1 issue you'd need:
+   * printf("You have %f. You are passing: %s\n", gpa, passing ? "true" : "false");
+   * Which uses a ternary operator as an if/else (if true print true else false)
+   */
 } 
