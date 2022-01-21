5bd30c85bdd078256b0ea0c7679341d007fb05e2
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2022-01-20 19:16:25 -0500

    Update cop2001Lecture1.cpp

--- cop2001Lecture1.cpp
+++ cop2001Lecture1.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 // Quick test of compiler
@@ -6,18 +5,18 @@
 // print out current gpa
 int main() {
 
+  // uid = university id
   int uid=125496;
   std::cout << "Hello student " << uid << "\n";
   printf("Hello student %d\n", uid);
 
+  // passing = "are you passing?" or > 3.0 gpa
   double gpa = 3.7;
   bool passing = true;
+  
+  // Using std library boolalpha to print (note its not passed as an argument)
   std::cout << "You have " << gpa << ". You are passing: "
-    << passing << "\n";
-  printf("You have %f. You are passing: %d\n", gpa, passing);
-  /*
-   * To fix the printf 1 issue you'd need:
-   * printf("You have %f. You are passing: %s\n", gpa, passing ? "true" : "false");
-   * Which uses a ternary operator as an if/else (if true print true else false)
-   */
-} 
+     << std::boolalpha << passing << "\n";
+  // or using printf with float truncation, its not rounding just cuts it off, and a ternary
+  // printf("You have %.1f. You are passing: %s\n", gpa, passing ? "true" : "false");
+}
