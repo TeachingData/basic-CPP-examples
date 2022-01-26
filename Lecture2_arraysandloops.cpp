d7dce810ee27f95af1345d0f6c83aed153927906
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2022-01-26 09:42:19 -0500

    Matched completed code to that done in class
    Didn't get to if/elsif/else so will have to do that on Thursday

--- Lecture2_arraysandloops.cpp
+++ Lecture2_arraysandloops.cpp
@@ -2,66 +2,57 @@
 #include <cstdlib>
 #include <array>
 
-// Usually wouldn't use std::string, just for example
-using std::cout, std::string, std::cin;
+using std::cout;
+using std::string;
+using std::cin;
 
 int main()
 {
-  // first version full - playing rock paper scissors
+  // first version only gets player choice - playing rock paper scissors
   // choices = available choices for both computer and player
-  const std::array<string,3> choices = {"rock", "paper", "scissors"};
+  const std::array<string,3> choices {"rock", "paper", "scissors"};
   // if above gives error - you need 17
 
   // select random value (using cstdlib)
   string computer = choices[rand() % 3];
   cout << computer << "\n";
+
   string player = "";
 
-  // Get player choice (keep looping until valid choice or player exits)
+  // Get player choice (keep looping until valid
   while (true) {
       // Give user all choices
       // Have to define leave here because see line 33 or so
-      string pchoice = "";
+      string confirm = "";
       for (const auto choice : choices) {
           cout << "Do you choose " + choice + "? (y/n)";
-          cin >> pchoice;
+          cin >> confirm;
 
           // Check if answer starts with Y or y: exit loop & assign
-          if (pchoice[0] == 'y' or pchoice[0] == 'Y') {
+          if (confirm[0] == 'y' or confirm[0] == 'Y') {
               player = choice;
               break;
           }
       }
       // Now we check if leave is still set
-      if (pchoice[0] == 'y' or pchoice[0] == 'Y') {
+      // This breaks out of while loop (moving to line 55)
+      if (confirm[0] == 'y' or confirm[0] == 'Y') {
           break;
       }
 
-      // Client did not make valid choice so ask if want to leave
+      // Client did not pick any of the valid choices so ask if want to leave
       string leave {}; // new scope so need new definition
       cout << "\nYou didn't select a choice - do you want to exit? (y/n)";
       cin >> leave;
 
-      // or use .tolower() function
+      // If they want to leave we just exit not break (cause done)
       if (leave[0] == 'y' or leave[0] == 'Y') {
         cout << "\nOkay, goodbye.";
         exit(0);
       }
   }
-  
-  // Check if we won: just check wining for now (students add ties/loss)
-  // Cannot use switch on strings so must be if/else (string doesn't have a hash ... map does though)
-  if (computer == "rock") {
-      if (player == "paper") {
-          cout << "You Win!\n";
-      }
-  } else if (computer == "paper") {
-      if (player == "scissors") {
-          cout << "You win!\n";
-      }
-  } else {
-      if (player == "rock") {
-          cout << "You win!\n";
-      }
-  }
+
+  // for now we are just going to print out our values - students will make if/else on Thurs
+  cout << "\n\t\tThe Player choose: " << player;
+  cout << "\n\t\tThe CPU choose: " << computer;
 }
