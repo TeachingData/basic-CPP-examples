d7c7e17386976679f00c380b65cf63e232e04270
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2022-03-31 19:36:14 -0400

    Update Mobile.cpp

--- MobileClassExample/Mobile.cpp
+++ MobileClassExample/Mobile.cpp
@@ -1,134 +1,73 @@
-//
-// Created by JGreenwell on 3/06/2022.
-//
-
 #include <string>
 #include <iostream>
 #include <random>
 #include <cmath>
+#include "Date.cpp"
+
+using std::string;
 
 class Mobile {
-    // in class we will make all members public then change
 private:
+    string OS {"Android"};
     int year;
     int month;
     int day;
-    std::string OS; // add this with case
+    int storage = 64;
+    string simtype = "physical"; // physical or esim
 
 public:
+    long sim_id;
     long IMEI;
-    long simid;
-    std::string simtype {"physical"}; // esim or physical
-    int storage {32}; // by Gb
+    string serialnum = "0000";
+    Date date = Date();
 
-    // Creating two constructors to introduce overloading (polymorphism)
+    // default constructor for class
     Mobile() {
-        // as nothing was passed we will just assign random IMEI and simid
-        // mt uses the mersenne twister algo to generate a more random num then rand
-        // Then we generate two instances of it and get the absolute values (can be negative)
         std::mt19937 gen32;
         IMEI = abs(gen32());
-        simid = abs(gen32());
+        sim_id = abs(gen32());
     }
 
-    Mobile(long imei, long simid) {
-        // This constructor just uses the passed values and the "this" construct
-        this->IMEI = imei;
-        this->simid = simid;
+    // Overloaded Constructor
+    Mobile(long imei) {
+        std::mt19937 gen32;
+        IMEI = imei;
+        sim_id = abs(gen32());
     }
 
-    // NOTE: set_date and set_date are considered overloaded functions because they have the same name
-    // but different parameters (part of polymorphism). This allows us to use a good descriptive name for 
-    // functions which do the same thing but using different input
-    void set_date(int month, int day, int year) {
-        // Overload pt2: this sets date with ints passed (standard way)
-        this->year = year;
-        this->month = month;
-        this->day = day;
+    // Overloaded Constructor
+    Mobile(long imei, long sim) {
+        IMEI = imei;
+        sim_id = sim;
     }
-    
-    // 1st overload: sets the date with a written month and int day/year: March 03 2022
-    void set_date(std::string month, int day, int year);
 
-    // final overloaded - it takes a full string and splits it - and inlined at bottom
-    // for "02/13/47" or "8--10--2022" or ...
-    void set_date(std::string date, std::string delimiter);
+    // getter and setters for private functions
 
-    int get_year() {return year; }
-    int get_month() {return month; }
-    int get_day() {return day; }
-
-    void setOS(std::string os) {
-        // We set the OS to either Android, Apple, Blackberry, or Symbian
-        // Symbian ended in 2013 with updates stopped in 2017 but still seen
-        // Note: Blackberry was bought out in 2020 and might make a 5G phone soon
-
-        // using if because all doing the same thing
-        if (os == "android" || os == "ios" ||
-            os == "symbian" || os == "blackberry") {
-            OS = os;
-        } else {
-            OS = "android";
+    // set and getter for simtype can only be esim or physical
+    void setsimtype(string st) {
+        if (st == "ESIM" || st == "esim" || st == "eSim") {
+            simtype = st;
         }
     }
+    string getsimtype() { return simtype; }
 
-    bool send_message(std::string message, long phonenum) {
-        std::cout << "We'd send a message here\n";
-        std::cout << "sending: " << message << "\nTo: " << phonenum << "\n";
+    // setter and getter for storage (must be multiple of 8)
+    void setstorage(int storage) {
+        if (storage % 8 == 0) {
+            // Mobile::storage = storage;
+            this->storage = storage;
+        }
+    }
+    int getstorage() { return storage; }
+
+    bool connect_wifi(string wifiname, string pass) {
+        // Connect to provided wifi with password
+        std::cout << "We connected!\n";
         return true;
     }
 
-    std::string receive_message() {
-        std::cout << "We'd get a message here\n";
-        return "And return it";
-    }
-
-    bool connect_wifi(std::string accessname, std::string password) {
-        std::cout << "We'll connect to wifi: " << accessname << " with pass " << password << "\n";
-        return true;
-    }
-
-    std::string dial(long phonenum) {
-        std::cout << "Dialing " << phonenum << "\n";
-        return "Hello?!";
+    void send_text(string txt) {
+        // send the message to someone and return if worked
+        std::cout << "You: " << txt << "\n";
     }
 };
-
-inline void Mobile::set_date(std::string month, int day, int year) {
-    this->day = day;
-    this->year = year;
-    
-    // run through month appriviations and assign a number as needed
-    const std::string months[] = {"jan", "feb", "mar", "apr", "may", 
-                                  "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
-    for (int i = 0; i <= 12; i++) {
-        if (months[i] == month) {
-            this->month = i+1;
-            // end loop cause we done
-            break;
-        }
-    }
-    
-}
-
-inline void Mobile::set_date(std::string date, std::string delimiter = "/") {
-    // Whereas this parses a string and splits it into its numbers
-    // "1/1/11" is smallest date so if smaller just exit
-    // note - using Europe style will mess this up - d/m/yy
-    if (date.length() > 6) {
-        int start = 0;
-        int end = date.find(delimiter);
-        int dates[] = {0,0,0};
-
-        for (int i = 0; i < 3; i++) {
-            // using stoi (from string library) to cast our string to int
-            dates[i] = stoi(date.substr(start, end - start));
-            start = end + delimiter.size();
-            end = date.find(delimiter, start);
-        }
-
-        month = dates[0];
-        day = dates[1];
-        year = dates[2];
-    }
-}
