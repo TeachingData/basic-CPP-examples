ca3b1b8784e4086c86efc9a0f6171c702b1b0292
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2023-04-17 19:55:15 -0400

    Create Files.hpp

---
+++ Inheritance/FileInheritance/Files.hpp
@@ -0,0 +1,19 @@
+#ifndef FILEINHERITANCE_FILES_HPP
+#define FILEINHERITANCE_FILES_HPP
+
+#include <string>
+#include <cctype>
+#include <fstream>
+#include <vector>
+
+class Files {
+public:
+    std::string filename;
+
+    virtual void change_filename(std::string)=0;
+    virtual void load_file()=0;
+    virtual void write_file(std::string)=0;
+};
+
+
+#endif
