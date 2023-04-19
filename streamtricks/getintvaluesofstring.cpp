9866a457aebd5eee5863fc3e2f5cc60c76bd09ff
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2023-04-19 14:11:39 -0400

    Update getintvaluesofstring.cpp

--- streamtricks/getintvaluesofstring.cpp
+++ streamtricks/getintvaluesofstring.cpp
@@ -2,10 +2,13 @@
 #include <sstream>
 
 // This shows how to get the int (char as int) values of a string
+// note this is just outputing the string but the standard use-case is to use it later
+// If you just want to output the string use ostringstream: https://cplusplus.com/reference/sstream/ostringstream/
 
 using std::string;
 using std::cout;
 using std::stringstream;
+using std::
 
 int main() {
   // change the string to whatever you want
@@ -20,7 +23,12 @@ int main() {
     numbers << (int) letter;  
   }
 
-  cout << "The integer representation of the string is " << numbers.str() << "\n";  
-  // using .str() to flush the buffer (convert it to string basically) and output it
+  cout << "The integer representation of the string is " << numbers.rdbuf() << "\n";  
+  // using .rdbuf() to flush the buffer (convert it to string basically) and output it
+  //       it actually just returns a pointer to the held data for output
+  //       see: https://cplusplus.com/reference/ios/ios/rdbuf/
   // prints '9811198' which are the ascii values of 'b' 'o' 'b'
+  // if you just want the string representation (i.e. convert it to a string) you'd use .str()
+  // functionally equivlent but rdbuf is made for output over str() which is made for getting the string for copy/move operations
+  cout << "Or I could print you with str " << numbers.str() << "\n";
 }
