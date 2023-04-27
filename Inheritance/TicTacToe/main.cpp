710705c31d79f52af4899e134ad5decdbdbce8e4
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2023-04-27 13:08:30 -0400

    Update main.cpp

--- Inheritance/TicTacToe/main.cpp
+++ Inheritance/TicTacToe/main.cpp
@@ -1,26 +1,14 @@
-#include <array>
+#include "Board.hpp"
 #include <iostream>
-
-void display_board(std::array<std::array<char, 3>, 3> &to_display) {
-  for (const auto &rows : to_display) {
-    for (const auto &column : rows) {
-      std::cout << column;
-    }
-    std::cout << "\n";
-  }
-}
 
 int main() {
 
-  /*std::array<std::array<char, 3>, 3> board = {{ {' ', ' ', ' '},
-                                               {' ', ' ', ' '},
-                                               {' ', ' ', ' '} }};*/
-
-  std::array<std::array<char, 3>, 3> board; // horrible use the above instead
+  // Now we add a class and setup the basic classes - students will build from here
+  Board b;
 
   int ctr = 0;
 
-  for (int i = 0; i < board.size() * board.at(0).size(); i++) {
+  for (int i = 0; i < b.board.size() * b.board.at(0).size(); i++) {
     int x, y;
 
     std::cout << "Please select a grid location x (0-2): ";
@@ -33,14 +21,15 @@ int main() {
       std::cout << error << "\n";
       exit(-1);
     }
+
     std::cout << "Please select a grid location y (0-2): ";
     std::cin >> y;
     if (i % 2) {
-      board[x][y] = 'O';
+      b.board[x][y] = 'O';
     } else {
-      board[x][y] = 'X';
+      b.board[x][y] = 'X';
     }
 
-    display_board(board);
+    b.display_board();
   }
 }
